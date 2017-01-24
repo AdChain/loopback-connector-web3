8e776bc11e74721369780b78adf21c6456d4cb31
Roman Lisagor <rlisagor@gmail.com>
2017-01-24 14:36:54 -0800

    Use async account fetch instead of sync
    This allows TestRPC to run in the same node process as the loopback app

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ var Web3 = require('./lib/web3');
 exports.initialize = function(dataSource, cb) {
   var settings = dataSource.settings;
   var connector = new Web3(settings);
+  connector.connect(cb);
+
   dataSource.connector = connector;
 };
